a7dfd532f53628995debde19179bcceb11d68deb
Niels Peter Rasmussen <nprasmussen@gmail.com>
2019-07-21 16:57:50 +0200

    load pin

--- src/views/Access.vue
+++ src/views/Access.vue
@@ -2,54 +2,70 @@
   <div class="access">
     <h1>Adgang til båden</h1>
     <p>Koden til båden er</p>
-    <input type="text" v-model="pin" disabled>
     <div>
-      <input type="number" v-model="pin0" min="0" max="9">
-      <input type="number" v-model="pin1" min="0" max="9">
-      <input type="number" v-model="pin2" min="0" max="9">
-      <input type="number" v-model="pin3" min="0" max="9">
+
+      <ul id="example-1">
+      <li v-for="pinCode in pinCodes">
+        {{ pinCode.pin }}, {{ pinCode.createdAt }}
+      </li>
+    </ul>
+      <title>{{ currentPin }}</title>
+      <input class="digit" type="number" v-model="currentPin[0]" min="0" max="9" disabled>
+      <input class="digit" type="number" v-model="currentPin[1]" min="0" max="9" disabled>
+      <input class="digit" type="number" v-model="currentPin[2]" min="0" max="9" disabled>
+      <input class="digit" type="number" v-model="currentPin[3]" min="0" max="9" disabled>  
+      <!-- <button @click="addPin">Update pin</button> -->
     </div>
+
+    
+
   </div>
 </template>
 
 
 <script lang="ts">
 import db from './../db';
+import firebase from 'firebase';
 
 export default {
   name: 'db',
   data() {
     return {
-      pin: '',
+      pinCodes: Array
     };
   },
-  beforeMount(){
-    this.getPin()
+  computed: {
+    // a computed getter
+    currentPin: function () {
+      return this.pinCodes[0].pin.split('');
+    }
+  },
+  firestore: {
+    pinCodes: db.collection('pinCode').orderBy('createdAt', "desc"),
   },
   methods: {
-    getPin: function() {
-   // retrieve a collection
+    addPin: function(pin) {
       db.collection('pinCode')
-        .get()
-        .then(
-          (querySnapshot) => {
-            const documents = querySnapshot.docs.map(doc => doc.data())
-            this.pin = documents[0].pin;
-            // do something with documents
-          }
-        ),
-        function(err) {
-          alert('Oops. ' + err.message)
-        }
+        .add({
+        pin: ""+this.pin0+this.pin1+this.pin2+this.pin3,
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      })
     }
   }
 }
 </script>
 <style>
 input {
-  margin: 10px 0;
+  margin: 10px 10px;
   width: 20%;
   padding: 15px;
+}
+.digit {
+  height: 50px;
+  width: 50px;
+  font-size: 20px;
+  text-align: center;
+  padding: 5px 0 5px 10px;
 }
 button {
   cursor: pointer;
