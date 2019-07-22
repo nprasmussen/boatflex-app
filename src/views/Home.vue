9b7197bdc515d4accab9c437a8d80e66c1232daf
Niels Peter Rasmussen <nprasmussen@gmail.com>
2019-07-22 21:29:37 +0200

    update images

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,15 +4,15 @@
     <p>I skuffen på bagbord side finder du nøglekort, 
       som kan bruges til at benytte toiletterne på havnekontoret. Der ligger også et sæt nøgle til kistebænkene.
     </p>
-    <img src="https://firebasestorage.googleapis.com/v0/b/sylvester-b0653.appspot.com/o/guide%2FIMG_1824.jpg?alt=media&token=06b005a3-be51-49e9-b41c-5efd0eea7ef7" alt="">
-    <img src="https://firebasestorage.googleapis.com/v0/b/sylvester-b0653.appspot.com/o/guide%2FIMG_1826.jpg?alt=media&token=f2914929-7a5c-4bba-a7bb-8c43a6e82898" alt="">
+    <img src="./img/instrukser/skuffe-til-nogler.jpg" alt="">
+    <img src="./img/instrukser/noglekort.jpg" alt="">
     
     <h3>Benzintank</h3>
     <p>I kistebænken på bagbord side er benzintanken. Åben først ventilen for at udligne trykket før tilslutning. Slut benzinslangen til ved at trække den igennem hullet bagerst i kistebænken og forbind den til tanken. Slut den derefter til motoren.
     </p>
     <div class="two-images">
-      <img src="https://firebasestorage.googleapis.com/v0/b/sylvester-b0653.appspot.com/o/guide%2FIMG_1834.jpg?alt=media&token=ab9df5b9-dfc4-41e4-934a-6e90ada4eb95" alt="">
-      <img src="https://firebasestorage.googleapis.com/v0/b/sylvester-b0653.appspot.com/o/guide%2FIMG_1833.jpg?alt=media&token=12fa7ec6-92f7-4979-bd0f-efc8e2212990" alt="">
+      <img src="./img/instrukser/benzin-tank-bank.jpg" alt="">
+      <img src="./img/instrukser/benzin-tank-ventil.jpg" alt="">
     </div>
     
     <p>Start motoren</p>
